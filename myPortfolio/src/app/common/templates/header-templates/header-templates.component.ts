6616cd220e280c347fada5a8a60cd582486c88a9
Rabin Ramarajan <100574003+Rabinramarajan@users.noreply.github.com>
2024-10-26 20:51:02 +0530

    Rabin

---
+++ myPortfolio/src/app/common/templates/header-templates/header-templates.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-templates',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-templates.component.html',
+  styleUrl: './header-templates.component.scss'
+})
+export class HeaderTemplatesComponent {
+
+}
