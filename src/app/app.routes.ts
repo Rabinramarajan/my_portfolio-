8bce09f3670e9d7473e5fc42820d3eeb8507b09d
Rabin Ramarajan <suriyarabin@gmail.com>
2024-11-05 01:56:17 +0530

    RAbin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,10 @@ export const routes: Routes = [
         path: '',
         loadComponent: () => import('./main/main').then((m) => m.MainComponent),
     },
-    { path: 'privacy-policy', component: PrivacyPolicyComponent },
+    {
+        path: 'privacy-policy',
+        loadComponent: () => import('./common/templates/privacy-policy').then((m) => m.PrivacyPolicyComponent),
+    },
     {
         path: '**',
         pathMatch: 'full',
